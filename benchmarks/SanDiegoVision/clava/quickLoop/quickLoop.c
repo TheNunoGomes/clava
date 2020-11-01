0b07343ff8263c1ef33d78d193b6a6597b823095
Nuno Gomes <32264676+TheNunoGomes@users.noreply.github.com>
2020-11-01 15:52:34 -0500

    feat: Loop Fission

---
+++ benchmarks/SanDiegoVision/clava/quickLoop/quickLoop.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+	
+	int j, i, N = 10;
+	int array[N][N];
+	int W = 3;
+	
+	//Write the matrix
+	for(i = 0; i < N; i++)
+	{
+		for(j = 0; j < N; j++)
+			array[i][j] = 2;
+	}
+
+	//Actual loop
+	for(i = 0 ; i < N ; ++i) 
+	{	
+		int start = i-W < 0 ? 0 : i-W;
+		int end = i+W < N-1 ? i+W : N-1;
+		for(j = start; j < end; j++)
+			array[i][j] += array[i][j-1] + 2;
+	}
+	
+	//Print Results
+	for(i = 0; i < N; i++)
+	{
+		for(j = 0; j < N; j++)
+			printf("Element [%d, %d] = %d", i, j, array[i][j]);
+	}
+
+	return 1;
+}
