72c7b4da1a65f150925bb01fee34d6e60943d047
Nuno Gomes <ngomes@hsdyn.com>
2020-12-14 19:49:46 +0000

    fix: W bug

--- benchmarks/SanDiegoVision/clava/manualTransformations/dissertation_vision/benchmarks/sift/src/c/imsmoothVersions/imsmooth_v5.c
+++ benchmarks/SanDiegoVision/clava/manualTransformations/dissertation_vision/benchmarks/sift/src/c/imsmoothVersions/imsmooth_v5.c
@@ -373,7 +373,7 @@ Author: Sravanthi Kota Venkata
 
 	clock_gettime(CLOCK_MONOTONIC, &clava_timing_end_0);
 	double clava_timing_duration_0 = ((clava_timing_end_0.tv_sec + ((double) clava_timing_end_0.tv_nsec / 1000000000)) - (clava_timing_start_0.tv_sec + ((double) clava_timing_start_0.tv_nsec / 1000000000))) * (1000);
-   	printf("W = %d\t\t\t %f\n", W, clava_timing_duration_0);
+   	printf("W = 5\t\t\t %f\n", clava_timing_duration_0);
     return;
   }
   
@@ -619,7 +619,7 @@ Author: Sravanthi Kota Venkata
 
 	clock_gettime(CLOCK_MONOTONIC, &clava_timing_end_0);
 	double clava_timing_duration_0 = ((clava_timing_end_0.tv_sec + ((double) clava_timing_end_0.tv_nsec / 1000000000)) - (clava_timing_start_0.tv_sec + ((double) clava_timing_start_0.tv_nsec / 1000000000))) * (1000);
-	printf("W = %d\t\t\t %f\n", W, clava_timing_duration_0);
+	printf("W = 7\t\t\t %f\n", clava_timing_duration_0);
     return;
   }
   
@@ -880,7 +880,7 @@ Author: Sravanthi Kota Venkata
 
 	clock_gettime(CLOCK_MONOTONIC, &clava_timing_end_0);
 	double clava_timing_duration_0 = ((clava_timing_end_0.tv_sec + ((double) clava_timing_end_0.tv_nsec / 1000000000)) - (clava_timing_start_0.tv_sec + ((double) clava_timing_start_0.tv_nsec / 1000000000))) * (1000);
-	printf("W = %d\t\t\t %f\n", W, clava_timing_duration_0);
+	printf("W = 8\t\t\t %f\n", clava_timing_duration_0);
     return;
   }
   
@@ -1170,7 +1170,7 @@ Author: Sravanthi Kota Venkata
 
 	clock_gettime(CLOCK_MONOTONIC, &clava_timing_end_0);
 	double clava_timing_duration_0 = ((clava_timing_end_0.tv_sec + ((double) clava_timing_end_0.tv_nsec / 1000000000)) - (clava_timing_start_0.tv_sec + ((double) clava_timing_start_0.tv_nsec / 1000000000))) * (1000);
-   	printf("W = %d\t\t\t %f\n", W, clava_timing_duration_0);
+   	printf("W = 10\t\t\t %f\n", clava_timing_duration_0);
     return;
   }
   
@@ -1497,7 +1497,7 @@ Author: Sravanthi Kota Venkata
 
 	clock_gettime(CLOCK_MONOTONIC, &clava_timing_end_0);
 	double clava_timing_duration_0 = ((clava_timing_end_0.tv_sec + ((double) clava_timing_end_0.tv_nsec / 1000000000)) - (clava_timing_start_0.tv_sec + ((double) clava_timing_start_0.tv_nsec / 1000000000))) * (1000);
-	printf("W = %d\t\t\t %f\n", W, clava_timing_duration_0);
+	printf("W = 13\t\t\t %f\n", clava_timing_duration_0);
     return;
   }
   
