02e63adce286ec00a704cea23dc69e2a6c5bad86
TheNunoGomes <nunomrgomes@hotmail.com>
2020-12-30 15:56:37 -0500

    disparity data reuse

--- benchmarks/SanDiegoVision/clava/manualTransformations/dissertation_vision/benchmarks/disparity/src/c/finalSAD.c
+++ benchmarks/SanDiegoVision/clava/manualTransformations/dissertation_vision/benchmarks/disparity/src/c/finalSAD.c
@@ -6,24 +6,23 @@ Author: Sravanthi Kota Venkata
 #include <stdlib.h>
 #include "disparity.h"
 
-void finalSAD(F2D* integralImg, int win_sz, F2D* retSAD)
+void finalSAD(F2D *__restrict__ integralImg, int win_sz, F2D *__restrict__ retSAD)
 {
     int endR, endC;
     int i, j, k;
     
-    endR = 1928;
-    endC = 1088;
+    endR = 1920;
+    endC = 1080;
     //printf("%dx%d\n", endR, endC);
     //printf("%d\n", win_sz);
-    
-    k = 0;
     
     float tl0, tl1, tl2, tl3, tl4, tl5, tl6, tl7;
     float tr0, tr1, tr2, tr3, tr4, tr5, tr6, tr7;
     float bl0, bl1, bl2, bl3, bl4, bl5, bl6, bl7;
-    float br0, br1, br2, br3, br4, br5, br6, bl7;
+    float br0, br1, br2, br3, br4, br5, br6, br7;
+    int count = 0, total = 0;
     
-    for(j=0; j<(endC-8); j++)
+    for(j=0; j<endC; j++)
     {
     	br0 = subsref(integralImg, 8, j+8);
     	tl0 = subsref(integralImg, 1, j+1);
@@ -60,6 +59,7 @@ void finalSAD(F2D* integralImg, int win_sz, F2D* retSAD)
     	tr6 = subsref(integralImg, 7, j+8);
     	bl6 = subsref(integralImg, 14, j+1);
     	
+    	
     	subsref(retSAD, 0, j) = br0 + tl0 - tr0 - bl0;
     	subsref(retSAD, 1, j) = br1 + tl1 - tr1 - bl1;
     	subsref(retSAD, 2, j) = br2 + tl2 - tr2 - bl2;
@@ -67,13 +67,68 @@ void finalSAD(F2D* integralImg, int win_sz, F2D* retSAD)
     	subsref(retSAD, 4, j) = br4 + tl4 - tr4 - bl4;
     	subsref(retSAD, 5, j) = br5 + tl5 - tr5 - bl5;
     	subsref(retSAD, 6, j) = br6 + tl6 - tr6 - bl6;
-    	
-        for(i=7; i<(endR-8); i++)
+            
+        for(i=7; i<endR; i++)
         {
-            subsref(retSAD,i,j) = subsref(integralImg,(8+i),(j+8)) + subsref(integralImg,(i+1) ,(j+1)) - subsref(integralImg,(i+1),(j+8)) - subsref(integralImg,(8+i),(j+1));
+            br7 = subsref(integralImg, i+8, j+8);
+            tl7 = subsref(integralImg, i+1, j+1);
+            tr7 = subsref(integralImg, i+1, j+8);
+            bl7 = subsref(integralImg, i+8, j+1);
+            
+            if(tl7 != bl0) {
+		printf("right: %f / %f\n", tr7, br0);
+		//printf("left: %f / %f\n", tl7, bl0);
+            	count++;
+            }
+            if(tr7 != br0) {
+		//printf("right: %f / %f\n", tr7, br0);
+		printf("left: %f / %f\n", tl7, bl0);
+            	count++;
+            }
+            
+            total++;
+            
+            subsref(retSAD,i,j) = br7 + tl7 - tr7 - bl7;
+            
+            
+            br0 = br1;
+            tl0 = tl1;
+            tr0 = tr1;
+            bl0 = bl1;
+            
+            br1 = br2;
+            tl1 = tl2;
+            tr1 = tr2;
+            bl1 = bl2;
+            
+            br2 = br3;
+            tl2 = tl3;
+            tr2 = tr3;
+            bl2 = bl3;
+            
+            br3 = br4;
+            tl3 = tl4;
+            tr3 = tr4;
+            bl3 = bl4;
+            
+            br4 = br5;
+            tl4 = tl5;
+            tr4 = tr5;
+            bl4 = bl5;
+            
+            br5 = br6;
+            tl5 = tl6;
+            tr5 = tr6;
+            bl5 = bl6;
+            
+            br6 = br7;
+            tl6 = tl7;
+            tr6 = tr7;
+            bl6 = bl7;
         }
     }
-
+    	
+	printf("%d / %d\n", count, total);
     return;
 }
 
