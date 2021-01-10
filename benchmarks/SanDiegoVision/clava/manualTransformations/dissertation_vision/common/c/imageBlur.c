06f53a0c2ddb44c6f8119de54071d59706a94636
TheNunoGomes <nunomrgomes@hotmail.com>
2021-01-10 11:43:19 -0500

    feat: tracking script

--- benchmarks/SanDiegoVision/clava/manualTransformations/dissertation_vision/common/c/imageBlur.c
+++ benchmarks/SanDiegoVision/clava/manualTransformations/dissertation_vision/common/c/imageBlur.c
@@ -1,5 +1,6 @@
 /********************************
 Author: Sravanthi Kota Venkata
+v3: Inline fSetArray and Specialized Matrix Size
 ********************************/
 
 #include "sdvbs_common.h"
@@ -9,62 +10,165 @@ F2D* imageBlur(I2D* imageIn)
     int rows, cols;
     F2D *imageOut, *tempOut;
     float temp;
-    I2D *kernel;
-    int k, kernelSize, startCol, endCol, halfKernel, startRow, endRow, i, j, kernelSum;
+    int endCol, endRow, i, j;
 
     rows = imageIn->height;
     cols = imageIn->width;
 
-    imageOut = fSetArray(rows, cols, 0);
-    tempOut = fSetArray(rows, cols, 0);
-    kernel = iMallocHandle(1, 5);
+    imageOut = fMallocHandle(rows, cols);
+    tempOut = fMallocHandle(rows, cols);
 
-    asubsref(kernel,0) = 1;
-    asubsref(kernel,1) = 4;
-    asubsref(kernel,2) = 6;
-    asubsref(kernel,3) = 4;
-    asubsref(kernel,4) = 1;
-    kernelSize = 5;
-    kernelSum = 16;
+    // VECTORIZADO
+    for(i=0; i<rows; i++) {
+	imageOut->data[(i) * cols + (0)] = 0;
+	imageOut->data[(i) * cols + (1)] = 0;
+	imageOut->data[(i) * cols + (cols-2)] = 0;
+	imageOut->data[(i) * cols + (cols-1)] = 0;
+	
+	tempOut->data[(i) * cols + (0)] = 0;
+	tempOut->data[(i) * cols + (1)] = 0;
+	tempOut->data[(i) * cols + (cols-2)] = 0;
+	tempOut->data[(i) * cols + (cols-1)] = 0;
+    }
+    for(j=0; j<cols; j++) {
+	imageOut->data[(0) * cols + (j)] = 0;
+	imageOut->data[(1) * cols + (j)] = 0;
+	imageOut->data[(rows-2) * cols + (j)] = 0;
+	imageOut->data[(rows-1) * cols + (j)] = 0;
+	
+	tempOut->data[(0) * cols + (j)] = 0;
+	tempOut->data[(1) * cols + (j)] = 0;
+	tempOut->data[(rows-2) * cols + (j)] = 0;
+	tempOut->data[(rows-1) * cols + (j)] = 0;
+    }
 
-    startCol = 2;  
-    endCol = cols - 2;  
-    halfKernel = 2;   
-
-    startRow = 2;    
+    endCol = cols - 2;
     endRow = rows - 2;  
-    // OPTIMIZED BASIC BLOCK VECTORIZED
-    for(i=startRow; i<endRow; i++) {
-	// VECTORIZED and UNROLLED
-        for(j=startCol; j<endCol; j++)
+    
+    float b0, b1, b2, b3, b4;
+    for(i=2; i<endRow; i++)
+    {
+    	b0 = subsref(imageIn,i,0);
+    	b1 = subsref(imageIn,i,1);
+    	b2 = subsref(imageIn,i,2);
+    	b3 = subsref(imageIn,i,3);
+        for(j=2; j<endCol; j++)
         {
-            temp = 0;
-            // UNROLLED
-            for(k=-halfKernel; k<=halfKernel; k++)
-            {
-                temp += subsref(imageIn,i,j+k) * asubsref(kernel,k+halfKernel);
-            }
-            subsref(tempOut,i,j) = temp/kernelSum;
+            b4 = subsref(imageIn,i,j+2);
+            
+            temp = b0;
+            temp += b1 * 4;
+            temp += b2 * 6;
+            temp += b3 * 4;
+            temp += b4;
+            
+            b0 = b1;
+            b1 = b2;
+            b2 = b3;
+            b3 = b4;
+            
+            subsref(tempOut,i,j) = temp*0.0625f;
         }
     }
     
-    for(i=startRow; i<endRow; i++)
+    for(i=2; i<endRow; i++)
     {
-	// VECTORIZED and UNROLLED
-        for(j=startCol; j<endCol; j++)
+    	// VECTORIZADO
+        for(j=2; j<endCol; j++)
         {
-            temp = 0;
-            // UNROLLED
-            for(k=-halfKernel; k<=halfKernel; k++)
-            {
-                temp += subsref(tempOut,(i+k),j) * asubsref(kernel,k+halfKernel);
-            }
-            subsref(imageOut,i,j) = temp/kernelSum;
+            temp = subsref(tempOut,(i-2),j);
+            temp += subsref(tempOut,(i-1),j) * 4;
+            temp += subsref(tempOut,(i),j) * 6;
+            temp += subsref(tempOut,(i+1),j) * 4;
+            temp += subsref(tempOut,(i+2),j);
+
+            subsref(imageOut,i,j) = temp*0.0625f;
         }
     }
 
     fFreeHandle(tempOut);
-    iFreeHandle(kernel);
+    return imageOut;
+}
+             
+F2D* imageBlur_1080x1920(I2D* imageIn)
+{
+    int rows = 1080, cols = 1920;
+    F2D *imageOut, *tempOut;
+    float temp;
+    int k, endCol, endRow, i, j;
+
+    imageOut = fMallocHandle(rows, cols);
+    tempOut = fMallocHandle(rows, cols);
+
+    // VECTORIZADO
+    for(i=0; i<rows; i++) {
+	imageOut->data[(i) * cols + (0)] = 0;
+	imageOut->data[(i) * cols + (1)] = 0;
+	imageOut->data[(i) * cols + (cols-2)] = 0;
+	imageOut->data[(i) * cols + (cols-1)] = 0;
+	
+	tempOut->data[(i) * cols + (0)] = 0;
+	tempOut->data[(i) * cols + (1)] = 0;
+	tempOut->data[(i) * cols + (cols-2)] = 0;
+	tempOut->data[(i) * cols + (cols-1)] = 0;
+    }
+    for(j=0; j<cols; j++) {
+	imageOut->data[(0) * cols + (j)] = 0;
+	imageOut->data[(1) * cols+ (j)] = 0;
+	imageOut->data[(rows-2) * cols + (j)] = 0;
+	imageOut->data[(rows-1) * cols + (j)] = 0;
+	
+	tempOut->data[(0) * cols + (j)] = 0;
+	tempOut->data[(1) * cols + (j)] = 0;
+	tempOut->data[(rows-2) * cols + (j)] = 0;
+	tempOut->data[(rows-1) * cols + (j)] = 0;
+    }
+    
+    endCol = cols - 2;
+    endRow = rows - 2;  
+    
+    float b0, b1, b2, b3, b4;
+    for(i=2; i<endRow; i++)
+    {
+    	b0 = subsref(imageIn,i,0);
+    	b1 = subsref(imageIn,i,1);
+    	b2 = subsref(imageIn,i,2);
+    	b3 = subsref(imageIn,i,3);
+        for(j=2; j<endCol; j++)
+        {
+            b4 = subsref(imageIn,i,j+2);
+            
+            temp = b0;
+            temp += b1 * 4;
+            temp += b2 * 6;
+            temp += b3 * 4;
+            temp += b4;
+            
+            b0 = b1;
+            b1 = b2;
+            b2 = b3;
+            b3 = b4;
+            
+            subsref(tempOut,i,j) = temp*0.0625f;
+        }
+    }
+    
+    for(i=2; i<endRow; i++)
+    {
+    	// VECTORIZADO
+        for(j=2; j<endCol; j++)
+        {
+            temp = subsref(tempOut,(i-2),j);
+            temp += subsref(tempOut,(i-1),j) * 4;
+            temp += subsref(tempOut,(i),j) * 6;
+            temp += subsref(tempOut,(i+1),j) * 4;
+            temp += subsref(tempOut,(i+2),j);
+
+            subsref(imageOut,i,j) = temp*0.0625f;
+        }
+    }
+
+    fFreeHandle(tempOut);
     return imageOut;
 }
              
