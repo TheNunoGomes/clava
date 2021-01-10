a089dd7a953742a356140e427aa5d10ef40af352
Nuno Gomes <nunomrgomes@gmail.com>
2021-01-10 18:58:15 +0000

    fix: remove stdlib

--- benchmarks/SanDiegoVision/clava/manualTransformations/dissertation_vision/benchmarks/disparity/src/c/integralImage2D2D.c
+++ benchmarks/SanDiegoVision/clava/manualTransformations/dissertation_vision/benchmarks/disparity/src/c/integralImage2D2D.c
@@ -3,7 +3,6 @@ Author: Sravanthi Kota Venkata
 ********************************/
 
 #include <stdio.h>
-#include <stdlib.h>
 #include "disparity.h"
 
 void integralImage2D2D(F2D* SAD, F2D* integralImg)
