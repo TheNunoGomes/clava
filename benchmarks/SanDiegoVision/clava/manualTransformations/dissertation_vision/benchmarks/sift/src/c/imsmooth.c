dff18aa9cff778d7215984d398a7b454a6a77ea6
TheNunoGomes <nunomrgomes@hotmail.com>
2021-01-30 19:48:49 -0500

    automated doubleToFloat

--- benchmarks/SanDiegoVision/clava/manualTransformations/dissertation_vision/benchmarks/sift/src/c/imsmooth.c
+++ benchmarks/SanDiegoVision/clava/manualTransformations/dissertation_vision/benchmarks/sift/src/c/imsmooth.c
@@ -113,3 +113,433 @@ void imsmooth(F2D* array, float dsigma, F2D* out)
     //printf("t - \t%.3f\n", (second - first)*1000);
   return;
 }
+
+void imsmooth5W(F2D* array, float dsigma, F2D* out)
+{
+  int M,N ;
+  int i,j,k;
+  float s ;
+    //double first, second;
+    // Save user and CPU start time
+    //first = CPU_TIME;
+  /* ------------------------------------------------------------------
+  **                                                Check the arguments
+  ** --------------------------------------------------------------- */ 
+
+    M = array->height;
+    N = array->width;
+    s = dsigma;
+    
+  /* ------------------------------------------------------------------
+  **                                                         Do the job
+  ** --------------------------------------------------------------- */ 
+  if(s > threshold) 
+  {
+    int W = 5;
+    float temp[2*W+1];
+    F2D* buffer;
+    float acc = 0.0;
+   
+    buffer = fSetArray(M,N,0);
+    
+    for(j = 0 ; j < (2*W+1) ; ++j) 
+    {
+      temp[j] = (float)(expf(-0.5 * (j - W)*(j - W)/(s*s))) ;
+      acc += temp[j];
+    }
+
+    for(j = 0 ; j < (2*W+1) ; ++j) 
+    {
+      temp[j] /= acc ;
+    }
+    
+    /*
+    ** Convolve along the columns
+    **/
+
+    for(j = 0 ; j < M ; ++j) 
+    {
+      for(i = 0 ; i < N ; ++i) 
+      {
+        int startCol = MAX(i-W,0);
+        int endCol = MIN(i+W, N-1);
+        int filterStart = MAX(0, W-i);
+        for(k=startCol; k<=endCol; k++)
+            subsref(buffer,j,i) += subsref(array, j, k) * temp[filterStart++];
+      }
+    }
+ 
+    /*
+    ** Convolve along the rows
+    **/
+    for(j = 0 ; j < M ; ++j) 
+    {
+      for(i = 0 ; i < N ; ++i) 
+      {
+        int startRow = MAX(j-W,0);
+        int endRow = MIN(j+W, M-1);
+        int filterStart = MAX(0, W-j);
+        for(k=startRow; k<=endRow; k++)
+            subsref(out,j,i) += subsref(buffer,k,i) * temp[filterStart++];
+      }
+    }  
+  
+    fFreeHandle(buffer);
+      
+  } 
+  else 
+  {
+      for(i=0;i<M*N;i++)
+          asubsref(out, i) = asubsref(array, i);
+  }
+
+
+    // Save end time
+    //second = CPU_TIME;
+    //printf("t - \t%.3f\n", (second - first)*1000);
+  return;
+}
+
+void imsmooth7W(F2D* array, float dsigma, F2D* out)
+{
+  int M,N ;
+  int i,j,k;
+  float s ;
+    //double first, second;
+    // Save user and CPU start time
+    //first = CPU_TIME;
+  /* ------------------------------------------------------------------
+  **                                                Check the arguments
+  ** --------------------------------------------------------------- */ 
+
+    M = array->height;
+    N = array->width;
+    s = dsigma;
+    
+  /* ------------------------------------------------------------------
+  **                                                         Do the job
+  ** --------------------------------------------------------------- */ 
+  if(s > threshold) 
+  {
+    int W = 7;
+    float temp[2*W+1];
+    F2D* buffer;
+    float acc = 0.0;
+   
+    buffer = fSetArray(M,N,0);
+    
+    for(j = 0 ; j < (2*W+1) ; ++j) 
+    {
+      temp[j] = (float)(expf(-0.5 * (j - W)*(j - W)/(s*s))) ;
+      acc += temp[j];
+    }
+
+    for(j = 0 ; j < (2*W+1) ; ++j) 
+    {
+      temp[j] /= acc ;
+    }
+    
+    /*
+    ** Convolve along the columns
+    **/
+
+    for(j = 0 ; j < M ; ++j) 
+    {
+      for(i = 0 ; i < N ; ++i) 
+      {
+        int startCol = MAX(i-W,0);
+        int endCol = MIN(i+W, N-1);
+        int filterStart = MAX(0, W-i);
+        for(k=startCol; k<=endCol; k++)
+            subsref(buffer,j,i) += subsref(array, j, k) * temp[filterStart++];
+      }
+    }
+ 
+    /*
+    ** Convolve along the rows
+    **/
+    for(j = 0 ; j < M ; ++j) 
+    {
+      for(i = 0 ; i < N ; ++i) 
+      {
+        int startRow = MAX(j-W,0);
+        int endRow = MIN(j+W, M-1);
+        int filterStart = MAX(0, W-j);
+        for(k=startRow; k<=endRow; k++)
+            subsref(out,j,i) += subsref(buffer,k,i) * temp[filterStart++];
+      }
+    }  
+  
+    fFreeHandle(buffer);
+      
+  } 
+  else 
+  {
+      for(i=0;i<M*N;i++)
+          asubsref(out, i) = asubsref(array, i);
+  }
+
+
+    // Save end time
+    //second = CPU_TIME;
+    //printf("t - \t%.3f\n", (second - first)*1000);
+  return;
+}
+
+void imsmooth8W(F2D* array, float dsigma, F2D* out)
+{
+  int M,N ;
+  int i,j,k;
+  float s ;
+    //double first, second;
+    // Save user and CPU start time
+    //first = CPU_TIME;
+  /* ------------------------------------------------------------------
+  **                                                Check the arguments
+  ** --------------------------------------------------------------- */ 
+
+    M = array->height;
+    N = array->width;
+    s = dsigma;
+    
+  /* ------------------------------------------------------------------
+  **                                                         Do the job
+  ** --------------------------------------------------------------- */ 
+  if(s > threshold) 
+  {
+    int W = 8;
+    float temp[2*W+1];
+    F2D* buffer;
+    float acc = 0.0;
+   
+    buffer = fSetArray(M,N,0);
+    
+    for(j = 0 ; j < (2*W+1) ; ++j) 
+    {
+      temp[j] = (float)(expf(-0.5 * (j - W)*(j - W)/(s*s))) ;
+      acc += temp[j];
+    }
+
+    for(j = 0 ; j < (2*W+1) ; ++j) 
+    {
+      temp[j] /= acc ;
+    }
+    
+    /*
+    ** Convolve along the columns
+    **/
+
+    for(j = 0 ; j < M ; ++j) 
+    {
+      for(i = 0 ; i < N ; ++i) 
+      {
+        int startCol = MAX(i-W,0);
+        int endCol = MIN(i+W, N-1);
+        int filterStart = MAX(0, W-i);
+        for(k=startCol; k<=endCol; k++)
+            subsref(buffer,j,i) += subsref(array, j, k) * temp[filterStart++];
+      }
+    }
+ 
+    /*
+    ** Convolve along the rows
+    **/
+    for(j = 0 ; j < M ; ++j) 
+    {
+      for(i = 0 ; i < N ; ++i) 
+      {
+        int startRow = MAX(j-W,0);
+        int endRow = MIN(j+W, M-1);
+        int filterStart = MAX(0, W-j);
+        for(k=startRow; k<=endRow; k++)
+            subsref(out,j,i) += subsref(buffer,k,i) * temp[filterStart++];
+      }
+    }  
+  
+    fFreeHandle(buffer);
+      
+  } 
+  else 
+  {
+      for(i=0;i<M*N;i++)
+          asubsref(out, i) = asubsref(array, i);
+  }
+
+
+    // Save end time
+    //second = CPU_TIME;
+    //printf("t - \t%.3f\n", (second - first)*1000);
+  return;
+}
+
+void imsmooth10W(F2D* array, float dsigma, F2D* out)
+{
+  int M,N ;
+  int i,j,k;
+  float s ;
+    //double first, second;
+    // Save user and CPU start time
+    //first = CPU_TIME;
+  /* ------------------------------------------------------------------
+  **                                                Check the arguments
+  ** --------------------------------------------------------------- */ 
+
+    M = array->height;
+    N = array->width;
+    s = dsigma;
+    
+  /* ------------------------------------------------------------------
+  **                                                         Do the job
+  ** --------------------------------------------------------------- */ 
+  if(s > threshold) 
+  {
+    int W = 10;
+    float temp[2*W+1];
+    F2D* buffer;
+    float acc = 0.0;
+   
+    buffer = fSetArray(M,N,0);
+    
+    for(j = 0 ; j < (2*W+1) ; ++j) 
+    {
+      temp[j] = (float)(expf(-0.5 * (j - W)*(j - W)/(s*s))) ;
+      acc += temp[j];
+    }
+
+    for(j = 0 ; j < (2*W+1) ; ++j) 
+    {
+      temp[j] /= acc ;
+    }
+    
+    /*
+    ** Convolve along the columns
+    **/
+
+    for(j = 0 ; j < M ; ++j) 
+    {
+      for(i = 0 ; i < N ; ++i) 
+      {
+        int startCol = MAX(i-W,0);
+        int endCol = MIN(i+W, N-1);
+        int filterStart = MAX(0, W-i);
+        for(k=startCol; k<=endCol; k++)
+            subsref(buffer,j,i) += subsref(array, j, k) * temp[filterStart++];
+      }
+    }
+ 
+    /*
+    ** Convolve along the rows
+    **/
+    for(j = 0 ; j < M ; ++j) 
+    {
+      for(i = 0 ; i < N ; ++i) 
+      {
+        int startRow = MAX(j-W,0);
+        int endRow = MIN(j+W, M-1);
+        int filterStart = MAX(0, W-j);
+        for(k=startRow; k<=endRow; k++)
+            subsref(out,j,i) += subsref(buffer,k,i) * temp[filterStart++];
+      }
+    }  
+  
+    fFreeHandle(buffer);
+      
+  } 
+  else 
+  {
+      for(i=0;i<M*N;i++)
+          asubsref(out, i) = asubsref(array, i);
+  }
+
+
+    // Save end time
+    //second = CPU_TIME;
+    //printf("t - \t%.3f\n", (second - first)*1000);
+  return;
+}
+
+void imsmooth13W(F2D* array, float dsigma, F2D* out)
+{
+  int M,N ;
+  int i,j,k;
+  float s ;
+    //double first, second;
+    // Save user and CPU start time
+    //first = CPU_TIME;
+  /* ------------------------------------------------------------------
+  **                                                Check the arguments
+  ** --------------------------------------------------------------- */ 
+
+    M = array->height;
+    N = array->width;
+    s = dsigma;
+    
+  /* ------------------------------------------------------------------
+  **                                                         Do the job
+  ** --------------------------------------------------------------- */ 
+  if(s > threshold) 
+  {
+    int W = 13;
+    float temp[2*W+1];
+    F2D* buffer;
+    float acc = 0.0;
+   
+    buffer = fSetArray(M,N,0);
+    
+    for(j = 0 ; j < (2*W+1) ; ++j) 
+    {
+      temp[j] = (float)(expf(-0.5 * (j - W)*(j - W)/(s*s))) ;
+      acc += temp[j];
+    }
+
+    for(j = 0 ; j < (2*W+1) ; ++j) 
+    {
+      temp[j] /= acc ;
+    }
+    
+    /*
+    ** Convolve along the columns
+    **/
+
+    for(j = 0 ; j < M ; ++j) 
+    {
+      for(i = 0 ; i < N ; ++i) 
+      {
+        int startCol = MAX(i-W,0);
+        int endCol = MIN(i+W, N-1);
+        int filterStart = MAX(0, W-i);
+        for(k=startCol; k<=endCol; k++)
+            subsref(buffer,j,i) += subsref(array, j, k) * temp[filterStart++];
+      }
+    }
+ 
+    /*
+    ** Convolve along the rows
+    **/
+    for(j = 0 ; j < M ; ++j) 
+    {
+      for(i = 0 ; i < N ; ++i) 
+      {
+        int startRow = MAX(j-W,0);
+        int endRow = MIN(j+W, M-1);
+        int filterStart = MAX(0, W-j);
+        for(k=startRow; k<=endRow; k++)
+            subsref(out,j,i) += subsref(buffer,k,i) * temp[filterStart++];
+      }
+    }  
+  
+    fFreeHandle(buffer);
+      
+  } 
+  else 
+  {
+      for(i=0;i<M*N;i++)
+          asubsref(out, i) = asubsref(array, i);
+  }
+
+
+    // Save end time
+    //second = CPU_TIME;
+    //printf("t - \t%.3f\n", (second - first)*1000);
+  return;
+}
